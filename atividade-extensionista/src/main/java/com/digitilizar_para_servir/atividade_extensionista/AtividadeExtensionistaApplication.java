fbff0066905c68fbae44ea30b6ae5e0d00bcf255
Junior Bortolanza <gbortolanzajr@gmail.com>
2026-08-16 20:22:27 -0300

    first commit

---
+++ atividade-extensionista/src/main/java/com/digitilizar_para_servir/atividade_extensionista/AtividadeExtensionistaApplication.java
@@ -0,0 +1,13 @@
+package com.digitilizar_para_servir.atividade_extensionista;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtividadeExtensionistaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtividadeExtensionistaApplication.class, args);
+	}
+
+}
---
+++ atividade-extensionista/src/test/java/com/digitilizar_para_servir/atividade_extensionista/AtividadeExtensionistaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.digitilizar_para_servir.atividade_extensionista;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtividadeExtensionistaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
